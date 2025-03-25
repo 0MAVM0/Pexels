6d5323bfa3d96b2e334907c32ee6e891a01619ae
0MAVM0 <ediconpts9@gmail.com>
2025-03-25 17:52:40 +0500

    Application (gallery) & Model (Pictures)

---
+++ config/gallery/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Pictures(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    image = models.ImageField(upload_to="images/", null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
