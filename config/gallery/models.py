976bb76d47833acf93e1a3e54b0ba8f051fabf82
0MAVM0 <ediconpts9@gmail.com>
2025-03-25 18:05:06 +0500

    Changing Name Of The Model

--- config/gallery/models.py
+++ config/gallery/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Pictures(models.Model):
+class Picture(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     image = models.ImageField(upload_to="images/", null=False)
     created_at = models.DateTimeField(auto_now_add=True)
