edf0687364a5bd2c8305b91bc5a8523ebaac9d39
0MAVM0 <ediconpts9@gmail.com>
2025-03-27 18:03:09 +0500

    image card

--- config/gallery/views.py
+++ config/gallery/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from .models import Picture
 
 def home_page(request):
-    return render(request, "home.html")
+    images = Picture.objects.all()
+    context = { "images" : images }
+
+    return render(request, "home.html", context)
